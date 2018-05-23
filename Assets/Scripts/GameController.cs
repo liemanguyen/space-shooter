8a6333602c895f19db960ae8d0321dcb36953216
Liem Nguyen <liemnguyen974@gmail.com>
2018-05-22 22:15:01 -0700

    extending space shooter

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -47,6 +47,7 @@ public class GameController : MonoBehaviour {
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
                 Vector3 spawnPosition = new Vector3(
                     Random.Range(-spawnValues.x, spawnValues.x),
                     spawnValues.y,
@@ -56,7 +57,6 @@ public class GameController : MonoBehaviour {
                 yield return new WaitForSeconds(spawnWait);
             }
             yield return new WaitForSeconds(waveWait);
-
             if (gameOver)
             {
                 restartText.text = "Press 'R' for Restart";
